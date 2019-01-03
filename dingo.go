12800263c8781a292647f163082f66ee9712534f
Thorsten Essig <te@tholutions.de>
2019-01-03 11:27:24 +0100

    fix nil pointer error on duplicate instance or provider binding
    to will be not set on these cases

--- dingo.go
+++ dingo.go
@@ -148,7 +148,14 @@ func (injector *Injector) InitModules(modules ...Module) {
 		known := make(map[string]*Binding)
 		for _, binding := range bindings {
 			if known, ok := known[binding.annotatedWith]; ok && !known.equal(binding) {
-				panic(fmt.Sprintf("already known binding for %s with annotation '%s' | Known binding: %#v%#v Try %#v%#v", typ, binding.annotatedWith, known.to.PkgPath(), known.to.Name(), binding.to.PkgPath(), binding.to.Name()))
+				var knownBinding, duplicateBinding string
+				if known.to != nil {
+					knownBinding = fmt.Sprintf("%#v%#v", known.to.PkgPath(), known.to.Name())
+				}
+				if binding.to != nil {
+					duplicateBinding = fmt.Sprintf("%#v%#v", binding.to.PkgPath(), binding.to.Name())
+				}
+				panic(fmt.Sprintf("already known binding for %s with annotation '%s' | Known binding: %s Try %s", typ, binding.annotatedWith, knownBinding, duplicateBinding))
 			}
 			known[binding.annotatedWith] = binding
 		}
