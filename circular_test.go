3d16a950cf49fa9e1bc849ab5bbb46245488065e
Bastian Ike <bastian.ike@gmail.com>
2019-04-17 18:11:53 +0200

    do not trace circular dependencies with Providers (#9)

--- circular_test.go
+++ circular_test.go
@@ -16,6 +16,17 @@ type (
 		A *circA `inject:""`
 		B *circB `inject:""`
 	}
+
+	circCProvider  func() circCInterface
+	circCInterface interface{}
+	circC          struct {
+		C circCProvider `inject:""`
+	}
+
+	circAProvider func() *circA
+	circD         struct {
+		A circAProvider `inject:""`
+	}
 )
 
 func TestDingoCircula(t *testing.T) {
@@ -30,5 +41,28 @@ func TestDingoCircula(t *testing.T) {
 		if !ok {
 			t.Fail()
 		}
+	})
+
+	injector.Bind(new(circCInterface)).To(circC{})
+	assert.NotPanics(t, func() {
+		c, ok := injector.GetInstance(new(circC)).(*circC)
+		if !ok {
+			t.Fail()
+		}
+
+		assert.NotNil(t, c.C())
+	})
+
+	var d *circD
+	assert.NotPanics(t, func() {
+		var ok bool
+		d, ok = injector.GetInstance(new(circD)).(*circD)
+		if !ok {
+			t.Fail()
+		}
+	})
+
+	assert.Panics(t, func() {
+		d.A()
 	})
 }
