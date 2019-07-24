8f9f5b4d8d0f21c8a6bb50a8e969c57035d09fb0
Bastian Ike <bastian.ike@aoe.com>
2019-07-24 11:13:12 +0200

    implement recursive module resolving
    resolves #11

--- module_test.go
+++ module_test.go
@@ -1,6 +1,7 @@
 package dingo
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -26,5 +27,43 @@ func TestTryModule(t *testing.T) {
 	err = TryModule(new(tryModuleFail))
 	if err == nil {
 		t.Errorf("tryModuleFail{} did not fail during module load, error: %q", err)
+	}
+}
+
+type (
+	resolveDependenciesModuleA  struct{}
+	resolveDependenciesModuleB  struct{}
+	resolveDependenciesModuleB2 struct{}
+	resolveDependenciesModuleC  struct{}
+)
+
+func (*resolveDependenciesModuleA) Configure(*Injector) {}
+func (*resolveDependenciesModuleA) Depends() []Module {
+	return []Module{
+		new(resolveDependenciesModuleA),
+		new(resolveDependenciesModuleB),
+		new(resolveDependenciesModuleB2),
+	}
+}
+func (*resolveDependenciesModuleB) Configure(*Injector) {}
+func (*resolveDependenciesModuleB) Depends() []Module {
+	return []Module{
+		new(resolveDependenciesModuleC),
+		new(resolveDependenciesModuleB2),
+	}
+}
+func (*resolveDependenciesModuleB2) Configure(*Injector) {}
+func (*resolveDependenciesModuleC) Configure(*Injector)  {}
+
+func Test_resolveDependencies(t *testing.T) {
+	resolved := resolveDependencies([]Module{new(resolveDependenciesModuleA)}, nil)
+
+	if !reflect.DeepEqual(resolved, []Module{
+		new(resolveDependenciesModuleC),
+		new(resolveDependenciesModuleB2),
+		new(resolveDependenciesModuleB),
+		new(resolveDependenciesModuleA),
+	}) {
+		t.Errorf("%#v not correctly resolved", resolved)
 	}
 }
